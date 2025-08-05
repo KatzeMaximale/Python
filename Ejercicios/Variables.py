daba13927a6790ea56f78c90f2a171a221299e0c
KatzeMaximale <eduardomendozare@gmail.com>
2025-08-04 22:53:49 -0600

    primera parte

---
+++ Ejercicios/Variables.py
@@ -0,0 +1,33 @@
+#No pueden empezar ni contener carácteres especiales
+#No pueden empezar por números
+#No pueden ser llamadas igual que las palabras claves reservadas en Python
+#No pueden contener espacios
+#No pueden contener símbolos especiales como @, #, $, etc.
+#No pueden ser iguales a los nombres de las funciones integradas de Python
+#'False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for',
+#'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+#tipos de variables
+# str: Cadenas de texto
+# int: Números enteros
+# float: Números decimales
+# bool: Valores booleanos (True o False)
+
+# Ejercicio 1: Crea una variable que contenga tu nombre
+
+tuNombre = "Andres"
+# Ejercicio 2: Crea una variable que contenga tu edad
+edad = 30
+# Ejercicio 3: Crea una variable que contenga tu altura en metros
+altura = 1.63
+# Ejercicio 4: Crea una variable que contenga si eres estudiante (True o False)
+estudiante = True
+
+#funcion import 
+import math as mt #as es un alias para math
+
+mt.tan #función tangente
+mt.pi #número pi
+
+
+
